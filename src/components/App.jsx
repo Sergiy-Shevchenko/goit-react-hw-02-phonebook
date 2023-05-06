41a965cb96180c86376eb8324b5987cdbcc39262
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-05-06 16:01:09 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+import css from './Fhonebook/fhonebook.module.css'
+
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  render () {
+return (
+  <div className={css.container}>
+    <div className="section">
+    <h2 className={css.title}>Phonebook</h2>
+    <form className={css.form}>
+      <h3 className={css.inputName}>Name</h3> 
+<input
+  className={css.input}
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
+<button type="submit" className={css.button}>Add contact</button>
+</form> 
+</div>
+<div className="section">
+<h2 className={css.title}>Contacts</h2>
+<ul className="contact__item">
+  <li className="contact__name"></li>
+</ul>
+</div>
+</div>
+)
+
+  }
+}
+
+export default App;
