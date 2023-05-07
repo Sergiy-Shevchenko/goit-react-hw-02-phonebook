8654ff0b67bed8f30523edc2572d0f86a096eca5
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-05-08 00:25:41 +0300

    4 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,56 @@
 import React, {Component} from "react";
-import css from './Fhonebook/fhonebook.module.css'
+import ContactsForm from "./Fhonebook/ContactsForm"
+import ContactsList from "./Fhonebook/ContactsList";
+import { Filter } from "./Fhonebook/Filter";
+import css from './Fhonebook/fhonebook.module.css';
+import { nanoid } from "nanoid";
 
 
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+   
+  };
+
+  addContact = nameData => {
+    const newContact = {
+      id: nanoid(),
+      ...nameData,
+    };
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({filter: event.currentTarget.value});
   }
 
-  render () {
-return (
-  <div className={css.container}>
-    <div className="section">
-    <h2 className={css.title}>Phonebook</h2>
-    <form className={css.form}>
-      <h3 className={css.inputName}>Name</h3> 
-<input
-  className={css.input}
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-/>
-<button type="submit" className={css.button}>Add contact</button>
-</form> 
-</div>
-<div className="section">
-<h2 className={css.title}>Contacts</h2>
-<ul className="contact__item">
-  <li className="contact__name"></li>
-</ul>
-</div>
-</div>
-)
+  
+  render() {
 
+const nopmalizedFilter = this.state.filter.toLocaleLowerCase();
+
+const visibleContact = this.state.contacts.filter(contact => 
+  contact.name.toLocaleLowerCase().includes(nopmalizedFilter));
+
+    return (
+      <div className={css.container}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactsForm addContactProps={this.addContact} />
+        <h2 className={css.title}>Contacts</h2>
+        <h3 className={css.inputName}>Find contacts by name</h3>
+        <Filter value={this.filter} onChange={this.changeFilter}/>
+        <ContactsList contacts={visibleContact} />
+        </div>
+    );
   }
 }
 
---
+++ src/components/Fhonebook/ContactsForm.jsx
@@ -0,0 +1,68 @@
+import React, {Component} from "react";
+import css from './fhonebook.module.css';
+import { nanoid } from "nanoid";
+import { ArrayType } from "proptype";
+
+
+class ContactsForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = ({target: {name, value}}) => {
+    // console.log(currentTarget.value);
+    this.setState({ [name]: value });
+  };
+
+  hendleSubmit = event => {
+    event.preventDefault();
+    this.props.addContactProps({ ...this.state });
+    console.log(this.state);
+
+    this.setState({
+      name: '',
+      number: '',
+  })
+  };
+
+  render() {
+    return (
+      <div className={css.section}>
+        <form className={css.form} onSubmit={this.hendleSubmit}>
+          <label className={css.inputName}>
+            Name
+            <input
+              className={css.input}
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={css.inputName}>
+            Number
+            <input
+              className={css.input}
+              type="tel"
+              value={this.state.number}
+              onChange={this.handleChange}
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit" className={css.button}>
+            Add contact
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactsForm;
